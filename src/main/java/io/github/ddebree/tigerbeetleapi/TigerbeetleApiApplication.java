a14890608bfa6922c25e88a95e1915e371680992
Dean de Bree <ddebree@gmail.com>
2024-03-14 21:16:00 +0200

    Create initial spring boot app

---
+++ src/main/java/io/github/ddebree/tigerbeetleapi/TigerbeetleApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.ddebree.tigerbeetleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerbeetleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerbeetleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ddebree/tigerbeetleapi/TigerbeetleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ddebree.tigerbeetleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerbeetleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
